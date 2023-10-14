cc7e79eab8325e3d2720e6e55d686bb7abbd9a34
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-10-14 16:02:18 +0700

    setup react navigation

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useCallback } from 'react';
+import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 function Login() {
+  const [fontsLoaded] = useFonts({
+    'Poppins-Medium': require('../../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('../../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Bold': require('../../assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <Text>Ini halam nlogin</Text>
+    <View onLayout={onLayoutRootView}>
+      <Text style={{
+        fontFamily: 'Poppins-Bold',
+        fontSize: 40,
+      }}
+      >
+        Ini halam nlogin
+      </Text>
+    </View>
   );
 }
 
