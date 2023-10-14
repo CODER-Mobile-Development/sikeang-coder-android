c12ca25e30f6b4745801203da2f44ae13c925b0e
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-10-14 14:34:41 +0700

    add folder structure

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sistem Pencatatan Keaktifan Anggota CODER</Text>
       <StatusBar style="auto" />
     </View>
   );
