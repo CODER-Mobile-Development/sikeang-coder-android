a274bf16772036a1358c91592ebc864dd233aae1
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-11-06 00:08:09 +0700

    Improve MemberProfile page

--- src/pages/MemberProfile/index.js
+++ src/pages/MemberProfile/index.js
@@ -1,8 +1,10 @@
 import React, { useCallback } from 'react';
-import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import {
+  Image, ScrollView, StyleSheet, Text, View,
+} from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { NavbarTop, PrimaryButton, Separator } from '../../components';
+import { NavbarTop, PrimaryButton } from '../../components';
 
 const poppinsMedium = require('../../assets/fonts/Poppins-Medium.ttf');
 const poppinsSemiBold = require('../../assets/fonts/Poppins-SemiBold.ttf');
@@ -11,127 +13,126 @@ const poppinsBold = require('../../assets/fonts/Poppins-Bold.ttf');
 SplashScreen.preventAutoHideAsync();
 
 function MemberProfile() {
-    const [fontsLoaded] = useFonts({
-        'Poppins-Medium': poppinsMedium,
-        'Poppins-SemiBold': poppinsSemiBold,
-        'Poppins-Bold': poppinsBold,
-    });
+  const [fontsLoaded] = useFonts({
+    'Poppins-Medium': poppinsMedium,
+    'Poppins-SemiBold': poppinsSemiBold,
+    'Poppins-Bold': poppinsBold,
+  });
 
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-            await SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded]);
-
-    if (!fontsLoaded) {
-        return null;
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
-    return (
-        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-            <NavbarTop title="" />
-            <View style={styles.wrapper}>
-                <Separator height={20} />
+  }, [fontsLoaded]);
 
-                <ScrollView style={styles.content}>
-                    <View style={styles.item}>
-
-                        <Text style={styles.Text}>
-                            Nama Lengkap
-                        </Text>
-                        <Text style={styles.Text1}>
-                            Dito Aditya Nugroho
-                        </Text>
-                    </View>
-                    <View style={styles.item}>
-
-                        <Text style={styles.Text}>
-                            Alamat Email
-                        </Text>
-                        <Text style={styles.Text1}>
-                            Dito@Gmail.com
-                        </Text>
-                    </View>
-                    <View style={styles.item}>
-
-                        <Text style={styles.Text}>
-                            Program Studi
-                        </Text>
-                        <Text style={styles.Text1}>
-                            Rekayasa Perangkat Lunak -2022
-                        </Text>
-                    </View>
-                    <View style={styles.item}>
-
-                        <Text style={styles.Text}>
-                            Nama divisi
-                        </Text>
-                        <Text style={styles.Text1}>
-                            Mobile Development
-                        </Text>
-                    </View>
-                    <View style={styles.item}>
-
-                        <Text style={styles.Text}>
-                            Jabatan DIvisi
-                        </Text>
-                        <Text style={styles.Text1}>
-                            Anggota
-                        </Text>
-                    </View>
-                </ScrollView>
-                <View style={styles.contentView}>
-                    <PrimaryButton title="Presensi Manual" />
-                </View>
-                <Text style={styles.Text2}>
-                    Versi 1.0.0
-                </Text>
-            </View>
-
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavbarTop title="Profile" />
+      <View style={styles.wrapper}>
+        <View style={styles.profileWrapper}>
+          <Image
+            source={{ uri: 'https://source.unsplash.com/random/120x120/?fruit' }}
+            width={40}
+            height={40}
+            style={{ borderRadius: 20, marginRight: 10 }}
+          />
+          <Text style={{ fontSize: 15, fontFamily: 'Poppins-Bold' }}>Irvan Surya Nugraha</Text>
         </View>
-    );
+        <ScrollView style={styles.content}>
+
+          <View style={styles.itemWrapper}>
+            <View style={{ width: '30%' }}>
+              <Text style={styles.titleText}>Alamat Email</Text>
+            </View>
+            <View style={{ width: '70%' }}>
+              <Text style={styles.valueText} ellipsizeMode="tail" numberOfLines={2}>irvan.surya.21@student.if.ittelkom-sby.ac.id</Text>
+            </View>
+          </View>
+
+          <View style={styles.itemWrapper}>
+            <View style={{ width: '30%' }}>
+              <Text style={styles.titleText}>Program Studi</Text>
+            </View>
+            <View style={{ width: '70%' }}>
+              <Text style={styles.valueText} ellipsizeMode="tail" numberOfLines={2}>INFORMATIKA</Text>
+            </View>
+          </View>
+
+          <View style={styles.itemWrapper}>
+            <View style={{ width: '30%' }}>
+              <Text style={styles.titleText}>Nama Divisi</Text>
+            </View>
+            <View style={{ width: '70%' }}>
+              <Text style={styles.valueText} ellipsizeMode="tail" numberOfLines={2}>Mobile Development</Text>
+            </View>
+          </View>
+
+          <View style={styles.itemWrapper}>
+            <View style={{ width: '30%' }}>
+              <Text style={styles.titleText}>Jabatan Divisi</Text>
+            </View>
+            <View style={{ width: '70%' }}>
+              <Text style={styles.valueText} ellipsizeMode="tail" numberOfLines={2}>Anggota</Text>
+            </View>
+          </View>
+
+        </ScrollView>
+        <View style={styles.logoutWrapper}>
+          <PrimaryButton title="Logout" />
+          <Text style={styles.versionText}>Versi 1.0.0</Text>
+        </View>
+      </View>
+
+    </View>
+  );
 }
 
 export default MemberProfile;
 
 const styles = StyleSheet.create({
-    wrapper: {
-        flex: 1,
-        justifyContent: 'space-between',
-        flexDirection: 'column',
-        backgroundColor: 'white',
-    },
-    content: {
-        marginTop: 20,
-        // paddingHorizontal: 35,
-    },
-    item: {
-        height: 77,
-        width: '100%',
-        flexDirection: 'row',
-        borderWidth: 1,
-        borderColor: '#A3A3A3',
-    },
-    Text: {
-        fontFamily: 'Poppins-Bold',
-        fontSize: 14,
-        marginTop: 17,
-        paddingHorizontal: 35,
-        width: '35%',
-    },
-    Text1: {
-        fontFamily: 'Poppins-SemiBold',
-        fontSize: 14,
-        marginTop: 17,
-        paddingHorizontal: 35,
-    },
-    Text2: {
-        fontFamily: 'Poppins-Bold',
-        fontSize: 14,
-        marginTop: 17,
-        textAlign: 'center',
-        marginBottom: 38,
-    },
-    contentView: {
-        paddingHorizontal: 35,
-    },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  content: {
+    marginTop: 10,
+    paddingHorizontal: 35,
+  },
+  profileWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+    paddingHorizontal: 35,
+  },
+  itemWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+  },
+  titleText: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 14,
+  },
+  valueText: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 14,
+    textAlign: 'right',
+  },
+  versionText: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 14,
+    marginTop: 10,
+    textAlign: 'center',
+    marginBottom: 38,
+  },
+  logoutWrapper: {
+    paddingHorizontal: 35,
+  },
 });
