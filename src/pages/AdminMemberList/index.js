68674c1667f8f864655b293b8a307cb9f7226403
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-11-06 17:28:21 +0700

    improve MemberlistView component and AdminMemberList page

--- src/pages/AdminMemberList/index.js
+++ src/pages/AdminMemberList/index.js
@@ -1,9 +1,12 @@
 import React, { useCallback } from 'react';
-import { ScrollView, StyleSheet, View, Text, Image } from 'react-native';
+import {
+  ScrollView, StyleSheet, Text, View,
+} from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { NavbarTop, PrimaryButton, Separator, SearchBar, MemberListView, NavbarBottom } from '../../components';
-import { AdminIcon } from '../../assets/svgs';
+import {
+  MemberListView, NavbarBottom, NavbarTop, PrimaryButton, SearchBar,
+} from '../../components';
 
 const poppinsMedium = require('../../assets/fonts/Poppins-Medium.ttf');
 const poppinsSemiBold = require('../../assets/fonts/Poppins-SemiBold.ttf');
@@ -12,57 +15,67 @@ const poppinsBold = require('../../assets/fonts/Poppins-Bold.ttf');
 SplashScreen.preventAutoHideAsync();
 
 function AdminMemberList() {
-    const [fontsLoaded] = useFonts({
-        'Poppins-Medium': poppinsMedium,
-        'Poppins-SemiBold': poppinsSemiBold,
-        'Poppins-Bold': poppinsBold,
-    });
+  const [fontsLoaded] = useFonts({
+    'Poppins-Medium': poppinsMedium,
+    'Poppins-SemiBold': poppinsSemiBold,
+    'Poppins-Bold': poppinsBold,
+  });
 
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-            await SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded]);
-
-    if (!fontsLoaded) {
-        return null;
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
-    return (
-        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-            <NavbarTop title="Divisi Mobile" />
-            <View style={styles.wrapper}>
-                <ScrollView style={styles.content}>
-                    <SearchBar placeholder={"cari nama anggota divisi"} />
-                    <Separator height={34} />
-                    <Text style={styles.Text}>Daftar Anggota</Text>
-                    <Separator height={16} />
-                    <MemberListView name="Dito" email="dito@example.com" photo={<AdminIcon width={24} height={24} isActive={true} />} />
-                    <Separator height={16} />
-                    <MemberListView name="Dito" email="dito@example.com" photo={<AdminIcon width={24} height={24} isActive={true} />} />
-                    <Separator height={336} />
-                    <PrimaryButton title="Tambah Anggota" />
-                </ScrollView>
-            </View>
-            <NavbarBottom type="Admin" isActive="Home" />
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavbarTop title="Divisi Mobile" />
+      <View style={styles.wrapper}>
+        <View style={{ paddingHorizontal: 35 }}>
+          <View style={{ paddingVertical: 20 }}>
+            <SearchBar placeholder="cari nama anggota divisi" />
+          </View>
+          <Text style={styles.titleText}>Daftar Anggota</Text>
         </View>
-    );
+        <ScrollView style={styles.content}>
+          <View style={{ gap: 10, paddingBottom: 20 }}>
+            <MemberListView name="Dito" email="dito@example.com" photo="https://source.unsplash.com/random/120x120/?fruit" />
+            <MemberListView name="Dito" email="dito@example.com" photo="https://source.unsplash.com/random/120x120/?fruit" />
+            <MemberListView name="Dito" email="dito@example.com" photo="https://source.unsplash.com/random/120x120/?fruit" />
+            <MemberListView name="Dito" email="dito@example.com" photo="https://source.unsplash.com/random/120x120/?fruit" />
+            <MemberListView name="Dito" email="dito@example.com" photo="https://source.unsplash.com/random/120x120/?fruit" />
+            <MemberListView name="Dito" email="dito@example.com" photo="https://source.unsplash.com/random/120x120/?fruit" />
+            <MemberListView name="Dito" email="dito@example.com" photo="https://source.unsplash.com/random/120x120/?fruit" />
+            <MemberListView name="Dito" email="dito@example.com" photo="https://source.unsplash.com/random/120x120/?fruit" />
+          </View>
+        </ScrollView>
+        <View style={{ paddingHorizontal: 35, marginVertical: 10 }}>
+          <PrimaryButton title="Tambah Anggota" />
+        </View>
+      </View>
+      <NavbarBottom type="Admin" isActive="Home" />
+    </View>
+  );
 }
 
 export default AdminMemberList;
 
 const styles = StyleSheet.create({
-    wrapper: {
-        flex: 1,
-        justifyContent: 'space-between',
-        flexDirection: 'column',
-        backgroundColor: 'white',
-    },
-    content: {
-        marginTop: 20,
-        paddingHorizontal: 35,
-    },
-    Text: {
-        fontFamily: 'Poppins-SemiBold',
-        fontSize: 16,
-    },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  content: {
+    marginTop: 5,
+    paddingHorizontal: 35,
+  },
+  titleText: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 16,
+  },
 });
