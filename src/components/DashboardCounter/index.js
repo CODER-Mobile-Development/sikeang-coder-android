9b16e6730c74ee8f4c0c5620d0d7518e68481d58
andiprtm <andipratama15011@gmail.com>
2023-11-06 20:18:48 +0700

    fix: change DashboardCounter title to dinamic title

--- src/components/DashboardCounter/index.js
+++ src/components/DashboardCounter/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 function DashboardCounter({
-  presencePoint, committeePoint, style,
+  presencePoint, committeePoint, style, rigthTitle, leftTitle,
 }) {
   return (
     <View style={{ ...styles.wrapper, ...style }}>
       <View style={styles.item}>
         <Text style={styles.pointSection}>{presencePoint}</Text>
-        <Text style={styles.pointTitle}>presensi</Text>
+        <Text style={styles.pointTitle}>{rigthTitle}</Text>
       </View>
       <View style={styles.divider} />
       <View style={styles.item}>
         <Text style={styles.pointSection}>{committeePoint}</Text>
-        <Text style={styles.pointTitle}>poin</Text>
+        <Text style={styles.pointTitle}>{leftTitle}</Text>
       </View>
     </View>
   );
