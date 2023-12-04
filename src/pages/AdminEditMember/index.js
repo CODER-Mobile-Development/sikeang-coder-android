274808c4a7275d29ffdd9503729a77accf6666e6
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-12-04 07:34:49 +0700

    added improvements
    - added divisionId fields in updateMemberUser
    - added deleteMemberUser functionality

--- src/pages/AdminEditMember/index.js
+++ src/pages/AdminEditMember/index.js
@@ -4,7 +4,14 @@ import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import {
-  DropdownInput, Loading, NavbarBottom, NavbarTop, PrimaryButton, Separator, UserInput,
+  DropdownInput,
+  Loading,
+  NavbarBottom,
+  NavbarTop,
+  OutlineButton,
+  PrimaryButton,
+  Separator,
+  UserInput,
 } from '../../components';
 import { API_HOST, CallAPI, showToast } from '../../utils';
 
@@ -80,7 +87,29 @@ function AdminEditMember({ route, navigation }) {
       userName: newUserName,
       email: newEmail,
       studyProgram: newStudyProgram,
+      divisionId: newDivision.data.id,
     });
+  };
+
+  const deleteMemberUserAPI = () => {
+    setLoadingScreen(true);
+    CallAPI({ url: `${API_HOST}/user/member/${_id}`, method: 'DELETE', data: null })
+      .then(() => {
+        setLoadingScreen(false);
+        navigation.goBack();
+      })
+      .catch(() => {
+        showToast(
+          'Gagal membuat data user, silahkan coba beberapa saat lagi!',
+          'danger',
+          insets.top,
+        );
+        setLoadingScreen(false);
+      });
+  };
+
+  const onDelete = () => {
+    deleteMemberUserAPI();
   };
 
   const onLayoutRootView = useCallback(async () => {
@@ -137,7 +166,19 @@ function AdminEditMember({ route, navigation }) {
             })}
           />
           )}
-          <Separator height={40} />
+          <Separator height={30} />
+          <OutlineButton
+            title="Hapus Anggota"
+            buttonStyle={{ borderWidth: 2, borderColor: '#8E8E8E', flex: 1 }}
+            textStyle={{ color: '#8E8E8E' }}
+            onPressIn={() => showToast(
+              'Tekan dan tahan 3 detik untuk menghapus data anggota!',
+              'info',
+              insets.top,
+            )}
+            onLongPress={onDelete}
+          />
+          <Separator height={7} />
           <PrimaryButton title="Simpan Perubahan" onPress={onSubmit} />
           <Separator height={40} />
         </ScrollView>
